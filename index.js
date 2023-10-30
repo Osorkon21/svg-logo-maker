4b12d739fcbef2dd3c990974960cee6e3e0cfb73
Osorkon21 <Osorkon21@users.noreply.github.com>
2023-10-30 01:15:13 -0500

    Add some frontend JS, add temporary .svg files

---
+++ index.js
@@ -0,0 +1,66 @@
+// import inquirer, node filesystem, and shapes.js 
+const inquirer = require("inquirer");
+const fs = require("fs");
+// const shapes = require("./lib/shapes.js");
+
+// questions for the command-line prompts
+const questions = [
+  {
+    type: "input",
+    message: "Enter your logo's text. You may use up to three characters.",
+    name: "logoText",
+    validate: (input) => input.length <= 3 ? true : "Three characters max - try again."
+  },
+  {
+    type: "input",
+    message: "Enter your logo's text color. Use CSS color keywords or a hexadecimal number.",
+    name: "textColor"
+  },
+  {
+    type: "list",
+    message: "Choose a shape for your logo.",
+    name: "shape",
+    choices: [
+      {
+        name: "Circle",
+        value: 0
+      },
+      {
+        name: "Triangle",
+        value: 1
+      },
+      {
+        name: "Square",
+        value: 2
+      }
+    ]
+  },
+  {
+    type: "input",
+    message: "Enter your logo's shape color. Use CSS color keywords or a hexadecimal number.",
+    name: "shapeColor"
+  }
+];
+
+// creates new logo.svg file in the output folder and writes its contents
+function writeSVG(contents) {
+  fs.writeFile("./output/logo.svg", contents, (err) => err ? console.error(err) : console.log("Generated logo.svg!"));
+}
+
+// prompts the user for input, generates SVG based on that input, and writes SVG to a new logo.svg file in the output folder
+async function init() {
+  const response = await inquirer.prompt(questions);
+  const contents = "blah blah";
+
+  // if (response.shape === 0)
+  //   contents = shapes.Circle.genSVG(response);
+  // else if (response.shape === 1)
+  //   contents = shapes.Triangle.genSVG(response);
+  // else
+  //   contents = shapes.Square.genSVG(response);
+
+  writeSVG(contents);
+}
+
+// function called when index.js is run
+init();
